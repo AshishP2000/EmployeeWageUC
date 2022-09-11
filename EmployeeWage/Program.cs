2ee830384001da5b8d657bb4431607b820d8481d
Ashish <ashish2000patil@gmail.com>
2022-09-11 15:48:52 +0530

    [Ashish]Added: Daily Employee Wage UC-2

--- EmployeeWage/Program.cs
+++ EmployeeWage/Program.cs
@@ -14,20 +14,27 @@ namespace EmployeeWage
         /// <param name="args"></param>
         static void Main(string[] args)
         {
-            //UC-1 Find Employee is Present or Absent
-            int empPresent = 1;
+            //UC-2 Calculate Daily Employee Wage
+            //we are not changing values of variable EMP_PRESENT and EMP_RATE_HR thats why we decalred it as constatnt
+            const int EMP_PRESENT = 1;
+            const int EMP_RATE_PER_HR = 20;
+            int empHrs = 0,empWage=0;
             Random random = new Random();
             //Generate
             int empCheck = random.Next(0, 2);
             Console.WriteLine("Random Value: " + empCheck);
-            if (empPresent == empCheck)
+            if (EMP_PRESENT == empCheck)
             {
+                empHrs = 8;
                 Console.WriteLine("Employee is Present");
             }
             else
             {
+                empHrs = 0;
                 Console.WriteLine("Employee is Absent");
             }
+            empWage = empHrs*EMP_RATE_PER_HR;
+            Console.WriteLine("Employee Wage per Day is: "+empWage);
             Console.ReadLine();
         }
     }
