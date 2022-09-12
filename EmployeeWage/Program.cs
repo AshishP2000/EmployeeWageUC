f23a491311571faf741b4a027ae805c956510c66
Ashish <ashish2000patil@gmail.com>
2022-09-12 22:01:56 +0530

    [Ashish]Added: Calculating Wages For Month UC-5

--- EmployeeWage/Program.cs
+++ EmployeeWage/Program.cs
@@ -14,35 +14,40 @@ namespace EmployeeWage
         /// <param name="args"></param>
         static void Main(string[] args)
         {
-            //UC-4 Using Switch Case
+            //UC-5 Calculating Wages for Month
             //we are not changing values of variable EMP_PRESENT and EMP_RATE_HR thats why we decalred it as constatnt
             const int FULL_TIME = 1;
             const int PART_TIME = 2;
             const int EMP_RATE_PER_HR = 20;
-            int empHrs = 0,empWage=0;
-            Random random = new Random();
-            //Generate
-            int empCheck = random.Next(0, 3);
-            Console.WriteLine("Random Value: " + empCheck);
-
-            switch (empCheck)
+            const int MAX_WORKING_DAYS = 20;
+            int empHrs = 0,empWage=0,totalEmpWage=0;
+            Random random = new Random();//Generate Random number
+            
+            for (int day = 1; day <= MAX_WORKING_DAYS; day++)
             {
-                case FULL_TIME:
-                    empHrs = 8;
-                    Console.WriteLine("Full Time Employee is Present");
-                    break;
-                case PART_TIME:
-                    empHrs = 4;
-                    Console.WriteLine("Part Time Employee is Present");
-                    break;
-                default:
-                    empHrs = 0;
-                    Console.WriteLine("Employee is Absent");
-                    break;
+                int empCheck = random.Next(0, 3);
+                //Console.WriteLine("Random Value: " + empCheck);
+                switch (empCheck)
+                {
+                    case FULL_TIME:
+                        empHrs = 8;
+                        //Console.WriteLine("Full Time Employee is Present");
+                        break;
+                    case PART_TIME:
+                        empHrs = 4;
+                        //Console.WriteLine("Part Time Employee is Present");
+                        break;
+                    default:
+                        empHrs = 0;
+                        //Console.WriteLine("Employee is Absent");
+                        break;
+                }
+                empWage = empHrs * EMP_RATE_PER_HR;
+                Console.WriteLine("Employee Wage per on day{0} is: {1} " ,day, empWage);
+                totalEmpWage += empWage;
             }
             
-            empWage = empHrs*EMP_RATE_PER_HR;
-            Console.WriteLine("Employee Wage per Day is: "+empWage);
+            Console.WriteLine("Employee Wage for Month is: "+ totalEmpWage);
             Console.ReadLine();
         }
     }
