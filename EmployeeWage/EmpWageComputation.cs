7c6b76921c530d2185a9475b74152ef98893202a
Ashish <ashish2000patil@gmail.com>
2022-09-17 15:23:33 +0530

    [Ashish]Added: Save Total Employee Wage for each Company UC-9

--- EmployeeWage/EmpWageComputation.cs
+++ EmployeeWage/EmpWageComputation.cs
@@ -13,13 +13,12 @@ namespace EmployeeWage
         /// </summary>
         /// <param name="args"></param>
         /// 
-        //UC-9 Ability to Save total Wage for each company
         public string COMPANY;
         public int EMP_RATE_PER_HR, MAX_WORKING_DAYS, MAX_WORKING_HRS;
 
         public EmpWageComputation()
         {
-            
+
         }
 
         public EmpWageComputation(string COMPANY, int EMP_RATE_PER_HR, int MAX_WORKING_DAYS, int MAX_WORKING_HRS)
@@ -27,20 +26,14 @@ namespace EmployeeWage
             this.COMPANY = COMPANY;
             this.EMP_RATE_PER_HR = EMP_RATE_PER_HR;
             this.MAX_WORKING_DAYS = MAX_WORKING_DAYS;
-            this.MAX_WORKING_HRS=MAX_WORKING_HRS;
+            this.MAX_WORKING_HRS = MAX_WORKING_HRS;
         }
-
-
-
 
         public void CalculateEmpWage()
         {
             //we are not changing values of variable EMP_PRESENT and EMP_RATE_HR thats why we decalred it as constatnt
             const int FULL_TIME = 1;
             const int PART_TIME = 2;
-            //const int EMP_RATE_PER_HR = 20;
-            //const int MAX_WORKING_DAYS = 20;
-            //const int MAX_WORKING_HRS = 100;
             int empHrs = 0, empWage = 0, totalEmpWage = 0, day = 1, totalHrs = 0;
             Random random = new Random();//Generate Random number
 
@@ -75,17 +68,5 @@ namespace EmployeeWage
             Console.WriteLine("Total Employee Wage is: " + totalEmpWage);
         }
 
-        static void Main(string[] args)
-        {
-            //UC-9 Ability to Save total Wage for each company
-
-            EmpWageComputation dmart = new EmpWageComputation("dmart", 20, 30, 120);
-            dmart.CalculateEmpWage();
-            EmpWageComputation reliance = new EmpWageComputation("relaince", 30, 40, 150);
-            reliance.CalculateEmpWage();
-
-
-            Console.ReadLine();
-        }
     }
 }
---
+++ EmployeeWage/program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWage
+{
+    public class program
+    {
+        
+        static void Main(string[] args)
+        {
+            //UC-9 Ability to Save total Wage for each company
+
+            EmpWageComputation dmart = new EmpWageComputation("dmart", 20, 30, 120);
+            dmart.CalculateEmpWage();
+            EmpWageComputation reliance = new EmpWageComputation("relaince", 30, 40, 150);
+            reliance.CalculateEmpWage();
+
+            Console.ReadLine();
+        }
+    }
+}
